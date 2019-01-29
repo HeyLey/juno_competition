007818d97d22104bf73e7f2a1a66ccd0cd6b2534
HeyLey <leila.xr@gmail.com>
2019-01-29 15:38:40 +0300

    add scoring program

---
+++ codalab/scoring_program/score.py
@@ -0,0 +1,45 @@
+import sys, os
+import yaml
+
+import numpy as np
+import pandas as pd
+
+from scipy.stats import ks_2samp
+
+
+scoring_program_path, input_dir, output_dir = sys.argv[1:]
+
+config_file_name = os.path.join(scoring_program_path, 'config.yml')
+
+with open(config_file_name) as f:
+    config = yaml.load(f)
+
+submit_dir = os.path.join(input_dir, 'res')
+reference_dir = os.path.join(input_dir, 'ref')
+
+reference_file = os.path.join(reference_dir, config['scoring_file'])
+prediction_file = os.path.join(submit_dir, config['prediction_file'])
+
+if not os.path.isdir(submit_dir):
+    print("{} doesn't exist".format(submit_dir))
+elif not os.path.isdir(reference_dir):
+    print("{} doesn't exist".format(reference_dir))
+else:
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    reference  = pd.read_csv(reference_file)
+    prediction = pd.read_csv(prediction_file)
+    prediction[config['X_cols']] = reference[config['X_cols']]
+
+    score = 1
+    cols = config['Y_cols'] + config['X_cols']
+    w_normal = np.random.normal(size=len(cols))
+    reference = reference[cols].values
+    prediction = prediction[cols].values
+   
+
+    output_filename = os.path.join(output_dir, 'scores.txt')
+
+    with open(output_filename, 'w') as output_file:
+        output_file.write("KS: {}".format(score))
