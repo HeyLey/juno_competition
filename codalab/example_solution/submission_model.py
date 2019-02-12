d1185b7ea8a37cca9e2b815b47f96ca3a9726906
HeyLey <leila.xr@gmail.com>
2019-02-13 00:44:34 +0300

    add comment

--- codalab/example_solution/submission_model.py
+++ codalab/example_solution/submission_model.py
@@ -5,6 +5,19 @@ import sys
 
 class Model:
     def run(self, pmts_pos, spmt_hits, lpmt_hist, true_info, test_l_hits, test_s_hits):
+
+        """
+        pmts_pos - positions of small and large PMTs
+        spmt_hits - train data with info for small PMTs hits
+        lpmt_hits - train data with info for large PMTs hits
+        true_info - train true info
+        test_l_hits - test data with info for large PMTs hits
+        test_s_hits - test data with info for small PMTs hits
+
+        Method returns dataframe with predictions.
+ 
+        """
+
         x = test_l_hits["event"].unique()
         pred = pd.DataFrame({
             "evtID": x,
